a6a05491e0dde6c0fe37dd1e51281a8b0d32c7dd
MbuguaMbuthi <mbuguambuti24@gmail.com>
2026-01-08 14:41:30 +0400

    Counter Data

--- JavaScript/counter.js
+++ JavaScript/counter.js
@@ -2,27 +2,49 @@ const incrementBtn = document.querySelector(".increment");
 const decrementBtn = document.querySelector(".decrement");
 const resetBtn = document.querySelector(".btn-danger");
 const display = document.getElementById("display");
+const round = document.querySelector(".roundDisplay");
 
 
 
 let count = 0;
+let counter = 0;
 
 incrementBtn.addEventListener("click", () => {
     count += 4;
     display.textContent = count;
 
+    counter++;
+    round.textContent = counter;
+
 
 });
 
 decrementBtn.addEventListener("click", () => {
-    count -= 4;
+
+    if (count === 0 || counter === 0) {
+        count = 0;
+        counter = 0;
+
+    }
+    else {
+
+        count -= 4;
+        counter--;
+
+    };
     display.textContent = count;
+    round.textContent = counter;
+
 
 
 });
 
 resetBtn.addEventListener("click", () => {
     count = 0;
+    counter = 0;
     display.textContent = count;
+    round.textContent = counter;
+
+
 
 });
