b039c84fc81c02acd6496d5dde7bdf9de1566537
MbuguaMbuthi <mbuguambuti24@gmail.com>
2026-01-24 19:16:40 +0400

    Version.1.0.0 commits

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -27,7 +27,7 @@ const routes = [
     { id: "R023", name: "Sharjah 2" },
     { id: "R024", name: "Abu Dhabi" },
     { id: "R025", name: "Jabel Ali 2" },
-    { id: "R026", name: "DTC" }, 
+    { id: "R026", name: "DTC" },
     { id: "R027", name: "Ras AL Khor" },
     { id: "R028", name: "Van 1/Mobile App" },
     { id: "R029", name: "Aquanim" },
@@ -35,12 +35,11 @@ const routes = [
 ];
 
 const items = [
-  
-    { key: "5 G Bottles", label: "5 G Bottles" },
-    { key: "5 G Bottles", label: "5 G Full Bottles" },
+
+    { key: "5 G full Bottles", label: "5 G Full Bottles" },
     { key: "5 G Empty Bottles", label: "5 G Empty Bottles" },
     { key: "5 G Damaged Bottles", label: "5 G Damaged Bottles" }
-   
+
 ];
 
 
@@ -61,33 +60,81 @@ routes.forEach(route => {
     routeSelect.appendChild(option)
 });
 
-
-const container = document.getElementById("itemContainer");
+const itemSelect = document.getElementById("itemSelect");
 items.forEach(item => {
-    const col = document.createElement("div");
-    col.className = "col-12 col-md-6 col-lg-6";
+    const itemOptions = document.createElement("option");
+    itemOptions.value = item.key.trim();
+    itemOptions.textContent = item.label.trim();
+    itemSelect.appendChild(itemOptions);
 
-    col.innerHTML = `
-        <div class="card h-100 shadow-sm border-1">
-            <div class="card-body p-2">
-                <label class="form-label fw-semibold small text-muted">
-                    ${item.label}
-                </label>
-                <input 
-                    type="number" 
-                    min="0" 
-                    value="0" 
-                    class="form-control text-center" 
-                    data-key="${item.key}">
-            </div>
-        </div>
-    `;
-
-    container.appendChild(col);
 });
 
+const qtyInput = document.getElementById("ewfItemsQty");
+const addBtn = document.getElementById("addItemBtn");
+const tableBody = document.getElementById("itemsTableBody");
+
+let selectedItems = [];
+
+addBtn.addEventListener("click", () => {
+
+    const item = itemSelect.value.trim();
+    const qty = Number(qtyInput.value);
+
+    if (!item || qty <= 0) {
+        alert("Please insert item and quantity");
+        return;
+
+    }
+    const existing = selectedItems.find(i => i.item === item);
+    if (existing) {
+        existing.qty += qty;
+    } else {
+        selectedItems.push({ item, qty });
+    }
+
+    renderItems();
+
+    itemSelect.value = "";
+    qtyInput.value = 1;
+});
+
+function renderItems() {
+    tableBody.innerHTML = "";
+
+    selectedItems.forEach((row, index) => {
+        const tr = document.createElement("tr");
+
+        const tdItem = document.createElement("td");
+        tdItem.textContent = row.item;
+
+        const tdQty = document.createElement("td");
+        tdQty.textContent = row.qty;
+        tdQty.classList.add("text-center");
+
+        const tdAction = document.createElement("td");
+        tdAction.classList.add("text-center");
+
+        const btn = document.createElement("button");
+        btn.className = "btn btn-danger btn-sm"
+        btn.type = "button";
+        btn.textContent = "X"
+        btn.addEventListener("click", () => {
+            selectedItems.splice(index, 1);
+            renderItems();
+        });
+
+        tdAction.appendChild(btn);
+
+        tr.append(tdItem, tdQty, tdAction);
+        tableBody.appendChild(tr);
+
+
+    });
+};
+
+
 const tripSelect = document.getElementById("tripSelect");
-deliveryTrips.forEach(trip =>{
+deliveryTrips.forEach(trip => {
     const tripOptions = document.createElement("option");
     tripOptions.value = trip.label;
     tripOptions.textContent = `Trip ${trip.label}`;
@@ -116,55 +163,52 @@ deliveryForm.addEventListener(
             return;
         }
 
-        const inputs = document.querySelectorAll("#itemContainer input[type='number']");
-        const itemsPayload = [];
-
-        inputs.forEach(input => {
-            const Qty = Number(input.value);
-            if (Qty > 0) {
-                itemsPayload.push({
-                    item_key: input.dataset.key,
-                    quantity: Qty
-                });
-            }
-        });
-
-        if (itemsPayload.length === 0) {
+        if (selectedItems.length === 0) {
             alert("Enter at least one item quantity");
             return;
         }
 
+        const itemsPayload = selectedItems.map(i => ({
+            item_key: i.item,
+            quantity: i.qty,
+        }));
+
+
         const payload = {
             timestamp: new Date().toISOString(),
             route_ID: selectedOption.value,
-            trip: tripSelect.value,
+            Trip: tripSelect.value,
             items: itemsPayload,
             driver: driver,
             Action: action,
             Enter_By: enteredBy
         };
 
-       try{
+        try {
             const response = await fetch(WEBHOOK_URL, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(payload)
-        });
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(payload)
+            });
 
 
-        if(!response.ok){
-        alert("Submission FAILED!");
-            
-            return;
-        };
-               
-        alert("Successfully Submitted");  
-        deliveryForm.reset();
-       }catch (error) {
-    console.error(error);
-    alert("Network error. Please try again.");
-}
-        
+            if (!response.ok) {
+                alert("Submission FAILED!");
+
+                return;
+            };
+
+            alert("Successfully Submitted");
+
+            selectedItems.length = 0;
+            renderItems();
+
+            deliveryForm.reset();
+        } catch (error) {
+            console.error(error);
+            alert("Network error. Please try again.");
+        }
+
     }
 );
 
---
+++ JavaScript/endwScript.js
@@ -0,0 +1,150 @@
+const WEBHOOK_URL = "";
+
+const routes = [
+    { id: "ER01", department: "ENDW", name: "Al Barsha" },
+    { id: "ER02", department: "ENDW", name: "Bur Dubai" },
+    { id: "ER03", department: "ENDW", name: "Deira 1" },
+    { id: "ER04", department: "ENDW", name: "Deira 2" },
+    { id: "ER05", department: "ENDW", name: "Ajman" },
+    { id: "ER06", department: "ENDW", name: "Sharjah 1" },
+    { id: "ER07", department: "ENDW", name: "Sharjah 2" },
+    { id: "ER08", department: "ENDW", name: "Credit Route Airport" },
+    { id: "ER09", department: "ENDW", name: "Credit Route 1" },
+    { id: "ER10", department: "ENDW", name: "Credit Route 2" },
+    { id: "ER11", department: "ENDW", name: "Credit Route 3" },
+    { id: "ER12", department: "ENDW", name: "Credit Route 4" },
+    { id: "ER13", department: "ENDW", name: "Credit Route 5" },
+    { id: "ER14", department: "ENDW", name: "Credit Route 6" },
+    { id: "ER15", department: "ENDW", name: "Credit Route 7" },
+    { id: "ER16", department: "ENDW", name: "Credit Route 8" },
+    { id: "ER17", department: "ENDW", name: "Credit Route 9" },
+    { id: "ER18", department: "ENDW", name: "Credit Route 10" }
+];
+
+const trips = [
+    { key: "Trip", label: "1" },
+    { key: "Trip", label: "2" },
+    { key: "Trip", label: "3" },
+    { key: "Trip", label: "4" },
+    { key: "Trip", label: "5" }
+
+];
+
+const items = [
+    { key: "ENDW", label: "1500M Carton" },
+    { key: "ENDW", label: "1500Ml Pack" },
+    { key: "ENDW", label: "500Ml Carton" },
+    { key: "ENDW", label: "500Ml Pack" },
+    { key: "ENDW", label: "330Ml Carton" },
+    { key: "ENDW", label: "330Ml Pack" },
+    { key: "ENDW", label: "250Ml Carton" },
+    { key: "ENDW", label: "250Ml Pack" },
+    { key: "ENDW", label: "250Ml Cup" },
+    { key: "ENDW", label: "200(36)Ml Cup " },
+    { key: "ENDW", label: "200(30)Ml Cup" },
+    { key: "ENDW", label: "150Ml Cup" },
+    { key: "ENDW", label: "100Ml Cup" },
+    { key: "ENDW", label: "100Ml Tray" },
+    { key: "ENDW", label: "Cooler" }
+
+
+];
+
+const routeSelect = document.getElementById("endwRouteSelect");
+routes.forEach(route => {
+    const options = document.createElement("option");
+    options.value = route.id;
+    options.textContent = route.name;
+    routeSelect.appendChild(options);
+});
+
+const tripSelect = document.getElementById("endwTripSelect");
+trips.forEach(trip => {
+    const tripOptions = document.createElement("option");
+    tripOptions.value = trip.label;
+    tripOptions.textContent = `Trip ${trip.label}`;
+    tripSelect.appendChild(tripOptions);
+});
+
+
+let brandHistory = [];// Remember to finish manipulating these two storage arrays
+let driverHistory = [];
+
+const itemSelect = document.getElementById("endwItemSelect");
+//const itemQty = document.getElementById("endwItemsQty");
+items.forEach(item => {
+    const itemOptions = document.createElement("option");
+    itemOptions.value = item.label;
+    itemOptions.textContent = item.label;
+    itemSelect.appendChild(itemOptions);
+
+});
+
+const qtyInput = document.getElementById("endwItemsQty");
+const addBtn = document.getElementById("addItemBtn");
+const tableBody = document.getElementById("itemsTableBody");
+
+let selectedItems = [];
+
+addBtn.addEventListener("click", () => {
+
+    const item = itemSelect.value;
+    const qty = Number(qtyInput.value);
+
+    if (!item || qty <= 0) {
+        alert("Please select an item and place a quantity");
+        return;
+
+    }
+    const existing = selectedItems.find(i => i.item === item);
+    if (existing) {
+        existing.qty += qty;
+    } else {
+        selectedItems.push({ item, qty });
+    }
+
+    renderItems();
+
+    itemSelect.value = "";
+    qtyInput.value = 1;
+});
+
+function renderItems() {
+    tableBody.innerHTML = "";
+
+    selectedItems.forEach((row, index) => {
+        const tr = document.createElement("tr");
+
+        const tdItem = document.createElement("td");
+        tdItem.textContent = row.item;
+
+        const tdQty = document.createElement("td");
+        tdQty.textContent = row.qty;
+        tdQty.classList.add("text-center")
+
+        const tdAction = document.createElement("td");
+        tdAction.classList.add("text-center");
+
+        const btn = document.createElement("button");
+        btn.className = "btn btn-danger btn-sm";
+        btn.type = "button";
+        btn.textContent = "X";
+
+        btn.addEventListener("click", () => {
+            selectedItems.splice(index, 1);
+            renderItems();
+        })
+
+        tdAction.appendChild(btn);
+
+        tr.append(tdItem, tdQty, tdAction);
+        tableBody.appendChild(tr);
+
+
+    });
+};
+
+
+
+
+
